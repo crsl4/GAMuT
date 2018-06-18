941129f0844d5b3144b68ad02260ea3e23bbfd8b
Anna Voss <anna.j.voss@emory.edu>
2018-06-18 11:41:39 -0400

    condensed

--- src/proj_GAMuT_pheno.jl
+++ src/proj_GAMuT_pheno.jl
@@ -2,8 +2,6 @@
 using NamedArrays
 using DataFrames
 function proj_GAMuT_pheno(x)
-    out = ["list", 2]
-    names(out) = ["kc", "ev_Kc"]
     kc = x*inv(At_mul_B(x, x))*x'
     ev_Kc = repeat([1], inner = size(x, 2))
     out = [kc, ev_Kc]
