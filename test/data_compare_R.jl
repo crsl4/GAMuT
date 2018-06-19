e711fc087d232a5cad11ef8f0b5ed72943946d91
Anna Voss <anna.j.voss@emory.edu>
2018-06-19 15:40:06 -0400

    updated data

--- test/data_compare_R.jl
+++ test/data_compare_R.jl
@@ -1,39 +1,44 @@
 using RCall
 R"""
-source("src/all_gamut_functions.r")
-source("test/simulation-one-edited.r")
+source("./src/all_gamut_functions.r")
+source("./test/simulation-one-edited.r")
 
-a = UNR_OBS
 
-x = linear_GAMuT_geno(a)
-y = TestGAMuT(x$Lc,x$ev_Lc,x$Lc,x$ev_Lc)
+x1 = linear_GAMuT_geno(Y1)
+x2 = linear_GAMuT_geno(Y2)
+y = TestGAMuT(x1$Lc,x1$ev_Lc,x2$Lc,x2$ev_Lc)
 """
+@rget Y1
+@rget Y2
+@rget x1
+@rget x2
 @rget y 
-@rget x
-@rget a
 
-include("src/linear_GAMuT_geno.jl")
-include("src/proj_GAMuT_pheno.jl")
-include("src/test_GAMuT.jl")
-lc,ev_Lc = linear_GAMuT_geno(a)
-y2 = testGAMuT(lc,ev_Lc,lc,ev_Lc)
+include("./src/linear_GAMuT_geno.jl")
+include("./src/proj_GAMuT_pheno.jl")
+include("./src/test_GAMuT.jl")
+lc,ev_Lc = linear_GAMuT_geno(Y1)
+lc_2,ev_Lc_2 = linear_GAMuT_geno(Y2)
+y2 = testGAMuT(lc,ev_Lc,lc_2,ev_Lc_2)
 
-all(isapprox.(x[:Lc],lc)) || error("julia and R functions do not match")
-all(isapprox.(x[:ev_Lc],ev_Lc)) || error("julia and R functions do not match")
+all(isapprox.(x1[:Lc],lc)) || error("julia and R functions do not match")
+all(isapprox.(x1[:ev_Lc],ev_Lc)) || error("julia and R functions do not match")
+all(isapprox.(x2[:Lc],lc_2)) || error("julia and R functions do not match")
+all(isapprox.(x2[:ev_Lc],ev_Lc_2)) || error("julia and R functions do not match")
 isapprox(y,y2) || error("julia and R functions do not match")
 
 
 R"""
-##Need square matrix
-b = replicate(500, rnorm(500))
-
-z = proj_GAMuT_pheno(b)
-w = TestGAMuT(z$Kc, z$ev_Kc, z$Kc, z$ev_Kc)
+z1 = proj_GAMuT_pheno(Y1)
+z2 = proj_GAMuT_pheno(Y1)
+w = TestGAMuT(z1$Kc, z1$ev_Kc, z2$Kc, z2$ev_Kc)
 """
-@rget b
+@rget Y1
+@rget Y2
 @rget w
 
-kc,ev_Kc = proj_GAMuT_pheno(b)
-w2 = testGAMuT(kc,ev_Kc,kc,ev_Kc)
+kc,ev_Kc = proj_GAMuT_pheno(Y1)
+kc_2,ev_Kc_2 = proj_GAMuT_pheno(Y2)
+w2 = testGAMuT(kc,ev_Kc,kc_2,ev_Kc_2)
 
 isapprox(w, w2) || error("julia and R functions do not match")
--- test/simulation-one-edited.r
+++ test/simulation-one-edited.r
@@ -1,19 +1,25 @@
 library(MASS)
 library(corpcor)
 
-#variables
+##------------------------------------------------------------------------------
+## Variables:  
+##------------------------------------------------------------------------------
 maf = 0.25 
-traitcor = med 
+traitcor = "med"
 nassoc1 = 1 
 nassoc2 = 1 
 npheno1 = 1 
 npheno2 = 1 
 n_unrelated = 5000 
 n_variants = 500
-test_approach = 1 
-simulation_approach = 3 
-ignore_Z = TRUE
+causal_var = 0.01
+test.approach = 1 
+sim.approach = 3 
+ignoreZ = TRUE
 
+##------------------------------------------------------------------------------
+## Preliminary Functions: 
+##------------------------------------------------------------------------------
 minorAlleleCountsBinomial = function(numtrios,MAF){
     mom1 = rbinom(numtrios, 1, MAF)
     mom1[mom1==0]=2 ##to match cosi
@@ -38,6 +44,180 @@ minorAlleleCountsBinomial = function(numtrios,MAF){
     eur_dad <- (eur[,3] == 1) + (eur[,4] == 1)
 
     return( list(G_kid=eur_kid, G_mom=eur_mom, G_dad=eur_dad) )
+}
+
+## function to create the phenotype covariance matrix
+## it takes as input the number of phenotypes: npheno, and the type of correlation:
+## "none","low", "med", "high"
+## "block" = 1-2, 3-4, "antiblock": only correlation btw phenotype matrices, not within
+createCovMatrix = function(npheno, traitcor){
+    if(traitcor == "block" || traitcor == "antiblock"){
+        if(npheno %% 2 != 0)
+            stop("cannot do block covariance matrix if npheno%%2 != 0")
+    }
+    if(traitcor == "none"){
+        phencor_ll <- 0
+        phencor_ul <- 0
+    }else if (traitcor=="low") {
+        phencor_ll <- 0
+        phencor_ul <- 0.3
+    } else {
+        if (traitcor=="med" || traitcor == "block" || traitcor == "antiblock") {
+            phencor_ll <- 0.3
+            phencor_ul <- 0.5
+        } else {
+            if (traitcor=="high") {
+                phencor_ll <- 0.5
+                phencor_ul <- 0.7
+            }
+        }
+    }
+    if(npheno == 1){
+        mat = as.matrix(runif(1,min=phencor_ll,max=phencor_ul))
+    }else{
+        if(traitcor != 'block' && traitcor != 'antiblock'){
+            cor = runif((npheno*(npheno-1))/2,min=phencor_ll,max=phencor_ul)
+            mat <- matrix(0, npheno,npheno)
+            mat[lower.tri(mat, diag=FALSE)] <- cor
+            mat = mat + t(mat) + diag(1, nrow=npheno)
+        }else{
+            if(traitcor == 'block'){
+                m = npheno/2
+                cor1 = runif((m*(m-1))/2,min=phencor_ll,max=phencor_ul)
+                mat1 <- matrix(0, m,m)
+                mat1[lower.tri(mat1, diag=FALSE)] <- cor1
+                mat1 = mat1 + t(mat1) + diag(1, nrow=m)
+                cor2 = runif((m*(m-1))/2,min=phencor_ll,max=phencor_ul)
+                mat2 <- matrix(0, m,m)
+                mat2[lower.tri(mat2, diag=FALSE)] <- cor2
+                mat2 = mat2 + t(mat2) + diag(1, nrow=m)
+                mat = blockMatrixDiagonal(mat1,mat2)
+            }else{
+                cor = runif((npheno*(npheno-1))/2,min=phencor_ll,max=phencor_ul)
+                mat1 <- matrix(0, npheno,npheno)
+                mat1[lower.tri(mat1, diag=FALSE)] <- cor
+                mat1 = mat1 + t(mat1) + diag(1, nrow=npheno)
+                mat = blockMatrixAntiDiagonal(mat1,npheno/2)
+            }
+        }
+    }
+    return(mat)
+}
+
+## function to set up the parameters to simulate the phenotypes associated with genotypes
+## it returns the beta matrix and the covariance matrix
+parameters4phenotypeSimulation = function(npheno, traitcor, causal.ind, nassoc, variant, MAF_unr){
+    if(npheno < nassoc)
+        stop("Error: npheno<nassoc")
+    MAF_C_unr<-MAF_unr[causal.ind]
+    ## pairwise similarity randomly generated from unif(phencor_ll, phencor_ul)
+    cov_unr= createCovMatrix(npheno, traitcor)
+    ## beta matrix: npheno by num of causal variants
+    betamat_unr <- matrix(0,nrow=npheno,ncol=length(causal.ind))
+    if(nassoc > 0){
+        hvec_unr<-rep(0.0,nassoc)
+        for (i in 1:nassoc) {
+            if (variant=="rare")
+                betamat_unr[i,] <- (0.4 + rnorm(length(causal.ind), 0, 0.1))*abs(log(MAF_C_unr, base=10))
+            else if (variant=="common")
+                betamat_unr[i,] <- rep(log(1.5),length(causal.ind))
+            hvec_unr[i] <- sum(betamat_unr[i,]^2*2*MAF_C_unr*(1-MAF_C_unr))
+        }
+
+        ## note: the first nassoc phenotypes are the ones that are associated with the genotype
+        for (i in 1:nassoc) {
+            for (ii in 1:nassoc) {
+                if (i==ii){
+                    cov_unr[i,ii] <- 1-hvec_unr[i]
+                } else if(ii>i) {
+                    cov_unr[i,ii] <- cov_unr[i,ii]*((1-hvec_unr[i])^0.5)*((1-hvec_unr[ii])^0.5)
+                    cov_unr[ii,i] <- cov_unr[i,ii]
+                }
+            }
+        }
+
+        if(!is.positive.definite(cov_unr))
+            cov_unr <- make.positive.definite(cov_unr)
+    }
+    return( list(betamat=betamat_unr, cov=cov_unr) )
+}
+
+simulatePhenotypes = function(npheno, traitcor, causal.ind, nassoc, variant, MAF, n_unrelated, G){
+    ## Creating parameters to simulate phenotype
+    out2 = parameters4phenotypeSimulation(npheno, traitcor, causal.ind, nassoc, variant, MAF)
+    betamat_unr = out2$betamat
+    cov_unr = out2$cov
+##    print(betamat_unr)
+##    print(cov_unr)
+##    print(nrow(UNR_OBS[1,causal.ind]))
+##    print(ncol(UNR_OBS[1,causal.ind]))
+    ## Actual phenotype simulation:
+    P0_UNR <- matrix(numeric(n_unrelated*npheno), ncol=npheno)
+    for(i in 1:n_unrelated)
+        P0_UNR[i,] <- mvrnorm(1,betamat_unr %*% G[i,causal.ind], cov_unr)
+    return( P0_UNR )
+}
+
+## Get Y1 and Y2 from phenotype matrix
+splitPhenotypeMatrix = function(P,nassoc1,nassoc2, npheno1,npheno2){
+    n = nrow(P)
+    m = ncol(P)
+    if(nassoc1>npheno1)
+        stop("Error: nassoc1>npheno1")
+    if(nassoc2>npheno2)
+        stop("Error: nassoc2>npheno2")
+    if(npheno1+npheno2 != m)
+        stop("Error: npheno1+npheno2 != npheno")
+
+    if(nassoc1 == 0){
+        Y2 = P[,1:npheno2]
+        Y1 = P[,(npheno2+1):m]
+    }else if(nassoc2 == 0){
+        Y1 = P[,1:npheno1]
+        Y2 = P[,(npheno1+1):m]
+    } else {
+        Y1 = P[,c(1:nassoc1,(nassoc1+nassoc2+1):(nassoc1+nassoc2+(npheno1-nassoc1)))]
+        Y2 = P[,c((nassoc1+1):(nassoc1+nassoc2),(nassoc1+nassoc2+npheno1-nassoc1+1):m)]
+    }
+    return(list(Y1=Y1,Y2=Y2))
+}
+
+## Each row in Y1 is simulated as normal with the row of Y2 as mean (assuming beta=1), and cov=I
+simulateFullyMediatedPhenotype = function(Y2,npheno1,n_unrelated,traitcor){
+    Y1 <- matrix(numeric(npheno1*n_unrelated), ncol=npheno1)
+    cov = createCovMatrix(npheno1, traitcor)
+    for(i in 1:n_unrelated){
+        if( ncol(Y2) <= npheno1 ){
+            mu = rep(0,npheno1)
+            for(j in 1:ncol(Y2))
+                mu[j] = Y2[i,j]
+        }else {
+            mu = Y2[i,1:npheno1]
+        }
+        Y1[i,] <- mvrnorm(1,mu, cov)
+    }
+    return( Y1 )
+}
+
+## Each row in Y1 is simulated as function of Y2 and G
+simulatePartiallyMediatedPhenotype = function(Y2,npheno1,n_unrelated, traitcor,causal.ind,nassoc1,variant,MAF_unr,UNR_OBS){
+    ## Creating parameters to simulate phenotype
+    out2 = parameters4phenotypeSimulation(npheno1, traitcor, causal.ind, nassoc1, variant, MAF_unr)
+    betamat_unr = out2$betamat
+    cov_unr = out2$cov
+    ## Actual phenotype simulation:
+    Y1 <- matrix(numeric(n_unrelated*npheno1), ncol=npheno1)
+    for(i in 1:n_unrelated){
+        if( ncol(Y2) <= npheno1 ){
+            mu = rep(0,npheno1)
+            for(j in 1:ncol(Y2))
+                mu[j] = Y2[i,j]
+        }else {
+            mu = Y2[i,1:npheno1]
+        }
+        Y1[i,] <- mvrnorm(1,betamat_unr %*% UNR_OBS[i,causal.ind]+mu, cov_unr)
+    }
+    return( Y1 )
 }
 
 simulatePhenotypesMediation = function(npheno1, npheno2,traitcor, nassoc1, nassoc2, causal.ind, MAF_unr, n_unrelated, variant,UNR_OBS, approach=1, numpcs=1, ignoreZ=FALSE){
